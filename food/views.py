8dced60ac9a36785cbf69f537229d806666d51d9
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-09-30 04:52:16 +0530

    Creating Templates

--- food/views.py
+++ food/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render 
 from django.http import HttpResponse 
-from .models import Item
+from .models import Item 
+from django.template import loader 
 
 # Create your views here.  
 
 def index(request):  
-    item_list = Item.objects.all()
-    return HttpResponse(item_list) 
+    item_list = Item.objects.all() 
+    context = { 
+ 
+          'item_list': item_list,
+    }
+    return render(request,'food/index.html',context) 
 
 def item(request): 
     return HttpResponse("This is an item view.")
