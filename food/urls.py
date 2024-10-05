5ce850afc94add9502d7aab1959b758bf926cca1
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-10-05 12:16:55 +0530

    Adding form to add items

--- food/urls.py
+++ food/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     # /food/1  
     path('<int:item_id>/',views.detail,name="detail"),
     path('item/', views.item , name="item"), 
+    path('add/', views.create_item , name="create_item"), 
    
 ]
